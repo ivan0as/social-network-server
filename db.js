12aa8b879de0af8e18ef0c3864c19c5b23b97b85
ivan0as <83854853+ivan0as@users.noreply.github.com>
2023-05-06 21:24:39 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-    }
-)
+module.exports = new Sequelize('postgres://fjaiixrp:9b4Jq-NTVu8S8306d7iWZU9AZIP0fvSu@lallah.db.elephantsql.com/fjaiixrp')
+
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//     }
+// )
