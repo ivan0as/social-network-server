60fc5ff3735b6b04301b0bfc0e0f9215217a2db3
ivan0as <83854853+ivan0as@users.noreply.github.com>
2023-05-09 19:45:54 +0700

    Update confirmFriendRouter.js

--- routers/confirmFriendRouter.js
+++ routers/confirmFriendRouter.js
@@ -5,5 +5,6 @@ const confirmFriendController = require('../controllers/confirmFriendController'
 router.get('/', confirmFriendController.getAll)
 router.post('/request_friendship', confirmFriendController.request)
 router.post('/acceptance/:id', confirmFriendController.acceptance)
+router.delete('/acceptance/:id', confirmFriendController.deleteApplication)
 
 module.exports = router
